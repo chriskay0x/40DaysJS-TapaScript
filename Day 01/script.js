b3411e545746ddb86257b8d210bd36fedcc4c056
Christian Kalu <christiankalu020@gmail.com>
2025-04-17 12:49:38 +0100

    Add files via upload

---
+++ Day 01/script.js
@@ -0,0 +1,6 @@
+console.log("Hello from Node.js");
+
+// Day 01 - Assignment
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript");
+document.write("Check the browser console for a message");
