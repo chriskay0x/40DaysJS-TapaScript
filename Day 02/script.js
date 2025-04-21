80c9193a3afb9d390eee1d3f81ad2f265943d751
Christian Kalu <christiankalu020@gmail.com>
2025-04-21 10:25:32 +0100

    Add files via upload

---
+++ Day 02/script.js
@@ -0,0 +1,26 @@
+const name = "Christian";
+let age = "32";
+const isStudent = true;
+let favouriteLanguage = "Javascript";
+
+console.log(
+    name,
+    age,
+    isStudent,
+    favouriteLanguage
+)
+
+console.log(`Hello, My name is ${name} and I'm ${age}, I'm currently into programming and my favourite language is ${favouriteLanguage}`);
+
+// Arrays
+let myArray = ["Christian", 32, "Blue", 0.5, "JavaScript"];
+console.log(myArray[2]);
+
+// Objects
+const bio = {
+    name: "Christian",
+    age: 32,
+    isStudent: true,
+    favouriteLanguage: "JavaScript"
+};
+console.log(bio.isStudent); 
